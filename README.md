34465479111f65e7e79abd3491b2310ab088d650
AcherStyx <AcherStyx@gmail.com>
2021-09-17 21:41:10 +0800

    basic function

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # kinetics-dataset-downloader
+
+Step 1. Download the dataset from [cvdfoundation/kinetics-dataset](https://github.com/cvdfoundation/kinetics-dataset) using the official download scrips.
+
+```bash
+bash download.sh k400_train_path.txt
+# extract
+bash extract.sh k400_train_path.txt
+```
+
+Step 2. Download new annotation (See the README.md from [cvdfoundation/kinetics-dataset](https://github.com/cvdfoundation/kinetics-dataset) to get the latest URL)
+
+```bash
+wget https://s3.amazonaws.com/kinetics/400/annotations/train.csv
+```
+
+Step 3. Run `make_subdirectory.py` to divide videos into subdirectories according to their label.
+
+
+```bash
+python make_subdirectory.py train.csv /path/to/video/folder
+```
---
+++ make_subdirectory.py
@@ -0,0 +1,35 @@
+import os
+import glob
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description="kinetics preprocess")
+
+parser.add_argument("csv", help="kinetics label file")
+parser.add_argument("videos", metavar="DIR", help="path to the video file")
+
+
+def load_label(csv):
+    table = np.loadtxt(csv, skiprows=1, dtype=str, delimiter=",")
+
+    return {k: v.replace('"', "") for k, v in zip(table[:, 1], table[:, 0])}
+
+
+def mv(file, subdir_class):
+    root_dir, f_name = os.path.split(file)
+    if not os.path.exists(os.path.join(root_dir, subdir_class)):
+        os.mkdir(os.path.join(root_dir, subdir_class))
+    os.rename(video_file, os.path.join(root_dir, subdir_class, f_name))
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+
+    label_dict = load_label(args.csv)
+
+    video_file_list = glob.glob(args.videos + "/*_*_*.mp4", recursive=False)
+
+    for video_file in video_file_list:
+        video_class = label_dict[os.path.split(video_file)[1][:11]]
+        mv(video_file, video_class)
